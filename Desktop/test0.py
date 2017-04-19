e83927e81d0f2a0f21eb1ff57277ef2ae91baccc
Sneha Vurrakula <snehavurrakula@gmail.com>
2017-04-18 17:54:36 -0700

    Updated cleaned files

--- Desktop/test0.py
+++ Desktop/test0.py
@@ -2,7 +2,6 @@ import requests
 import socket
 import sqlite3
 import re
-#from flask import Flask, request, jsonify
 
 host = ''
 port = 8080
@@ -16,16 +15,14 @@ while True:
 	print("Connection from:" + 'caddr')
 	req = csock.recv(1024)
 	#print (req)
-	#match = re.match('GET ^\/[/.a-zA-Z0-9-]+$', req)
 	userinp = req.split()[1]
     	if userinp:
-        	#userinp = match.group(1)
         	print ("url to check: " + userinp + "\n")
 		con = sqlite3.connect('mydata.db')
 		cur = con.cursor()
 		cur.execute("SELECT boolean_rep FROM testtable WHERE url_name =?", (userinp[1:],))
 		res = cur.fetchone()
-		print(res[0])
+		#print(res[0])
 		con.close()
 		if res[0] =="true" :
 			print("Malware found, not safe to access")
@@ -56,7 +53,7 @@ while True:
 			</html>
 				""")
     	else:
-      			# If there was no recognised command then return a 404 (page not found)
+      		# If there was no recognised command then return a 404 (page not found)
        		print "Returning 404"
        		csock.sendall("HTTP/1.0 404 Not Found\r\n")
     		csock.close()
